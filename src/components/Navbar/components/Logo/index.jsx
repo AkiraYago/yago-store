5ba0fbd8ac283606eafd20242d632a36b6d244e2
Yago E <the.programmer.yago@gmail.com>
2024-03-06 09:52:44 -0300

    update logo from navbar component

--- src/components/Navbar/components/Logo/index.jsx
+++ src/components/Navbar/components/Logo/index.jsx
@@ -1,9 +1,16 @@
-const Logo = () => {
+// Styles
+import styles from "./Logo.module.css";
+
+// React Router DOM
+import { Link } from "react-router-dom";
+
+const Logo = ({ img = "" }) => {
   return (
-    <div>
-      Logo
-    </div>
+    <Link className={styles.logoContainer} to="/">
+      <img className={styles.logoImg} src={img} alt="Imagen del Logo" />
+      <h1 className={styles.storeName}>YagoStore</h1>
+    </Link>
   );
-};
+}
 
 export default Logo;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,20 @@
 // Components
 import Navbar from "../../components/Navbar";
 
+// Assets
+import logoImg from "/yagostore-logo.svg"
+
 const Home = () => {
   return (
     <>
-      <Navbar />
+      {
+      /* Debo pasarle:
+        - logo img (logoProps)
+        - lista de categorias (categoriesProps)
+        - lista de la API de la tienda (searchBarProps)
+      */
+      }
+      <Navbar logoImg={logoImg} />
     </>
   );
 }
